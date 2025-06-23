1450db36f2c767ff463645c99b8c2069e326c5c3
Decent-Nazim <muhammadnazimrind@gmail.com>
2025-06-23 14:38:23 +0500

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n ⏰ Activated since ${muptime}\n\n ┃ PRINCE MD❤️ \n ${botname}`
+		let bio = `\n ⏰ Activated since ${muptime}\n\n ┃ NazimX-MD❤️ \n ${botname}`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
   }
